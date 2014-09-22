f63220ea491d3bb30ef866b3d49d111131a32746
Matt Langlois <fletchto99@gmail.com>
2014-09-21 23:22:22 -0400

    MOAR PIXLES PLS

--- src/me/matt/mousefx/effect/pixolution/Pixolution.java
+++ src/me/matt/mousefx/effect/pixolution/Pixolution.java
@@ -10,42 +10,44 @@ import me.matt.mousefx.effect.pixolution.particle.ColorParticle;
 import me.matt.mousefx.entity.Entity;
 
 public class Pixolution extends MouseEffect {
-	private List<Entity> entities = new ArrayList<>();
+    private List<Entity> entities = new ArrayList<>();
 
-	public void render(Graphics graphics) {
-		update();
-		for (int i = 0; i < entities.size(); i++) {
-			Entity entity = entities.get(i);
-			if (entity != null) {
-				entity.render(graphics);
-			}
-		}
-	}
+    public void render(Graphics graphics) {
+        update();
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            if (entity != null) {
+                entity.render(graphics);
+            }
+        }
+    }
 
-	public void update() {
-		for (int i = 0; i < entities.size(); i++) {
-			Entity entity = entities.get(i);
-			if ((entity == null) || (!entity.isAlive())) {
-				entities.remove(entity);
-			}
-		}
-		for (int i = 0; i < entities.size(); i++) {
-			Entity entity = entities.get(i);
+    public void update() {
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            if ((entity == null) || (!entity.isAlive())) {
+                entities.remove(entity);
+            }
+        }
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
 
-			if (entity != null) {
-				entity.update();
-			}
-		}
-	}
+            if (entity != null) {
+                entity.update();
+            }
+        }
+    }
 
-	@Override
-	public void mouseMoved(MouseEvent event) {
-		entities.add(new ColorParticle(event.getX(), event.getY()));
-	}
+    @Override
+    public void mouseMoved(MouseEvent event) {
+        for (int i = 0; i < 20; i++) {
+            entities.add(new ColorParticle(event.getX(), event.getY()));
+        }
+    }
 
-	@Override
-	public void mouseDragged(MouseEvent event) {
-		entities.add(new ColorParticle(event.getX(), event.getY()));
-	}
+    @Override
+    public void mouseDragged(MouseEvent event) {
+        entities.add(new ColorParticle(event.getX(), event.getY()));
+    }
 
 }
